a659a7cd94afc8d14e15744de3b4c89e16c119e2
Paul <1580247085@qq.com>
2019-02-26 18:33:57 +0800

    add new methods

--- www/orm.py
+++ www/orm.py
@@ -77,10 +77,10 @@ def create_args_string(num):
 # Field
 class Field(object):
     def __init__(self, name, column_type, primary_key, default):
-        self.__name = name
-        self.__column_type = column_type
-        self.__primary_key = primary_key
-        self.__default = default
+        self.name = name
+        self.column_type = column_type
+        self.primary_key = primary_key
+        self.default = default
 
     def __str__(self):
         return '<%s, %s:%s>' % (self.__class__.__name__, self.__column_type, self.__name)
@@ -128,9 +128,33 @@ class ModelMetaclass(type):
         mappings = dict()
         fields = []
         primarykey = None
-
-
-
+        for k, v in attrs.items():
+            if isinstance(v, Field):
+                logging.info('  Found mapping: %s==>%s' % (k, v))
+                mappings[k] = v
+                if v.primary_key:
+                    if primarykey:
+                        raise Exception('Duplicate primary key for field: %s' % k)
+                    primarykey = k
+                else:
+                    fields.append(k)
+        if not primarykey:
+            raise Exception('Primary key not found.')
+        for k in mappings.keys():
+            attrs.pop(k)
+        escaped_fields = list(map(lambda f: '`%s`' % f, fields))
+        attrs['__mapping__'] = mappings
+        attrs['__table__'] = tableName
+        attrs['__primary_key__'] = primarykey
+        attrs['__fields__'] = fields
+        attrs['__select__'] = 'select `%s`, %s from `%s`' % (primarykey, ', '.join(escaped_fields), tableName)
+        attrs['__insert__'] = 'insert into `%s` (%s, `%s`) value (%s)' % (tableName, ', '.join(escaped_fields), primarykey,
+                                                                          create_args_string(len(escaped_fields) + 1))
+        attrs['__update__'] = 'update `%s` set %s where `%s`=?' % (tableName,
+                                                                   ', '.join(map(lambda f: '`%s`=?' % (mappings.get(f).name or f),
+                                                                                 fields)), primarykey)
+        attrs['__delete__'] = 'delete from `%s` where `%s`=?' % (tableName, primarykey)
+        return type.__new__(cls, name, bases, attrs)
 
 
 # ORM
@@ -160,3 +184,88 @@ class Model(dict, metaclass=ModelMetaclass):
             logging.debug('using default value for %s: %s' % (key, str(value)))
             setattr(self, key, value)
         return value
+
+    @classmethod
+    async def findAll(cls, where=None, args=None, **kw):
+        """ find object by where clause """
+        sql = [cls.__select__]
+        if where:
+            sql.append('where')
+            sql.append(where)
+        if args is None:
+            args = []
+        orderBy = kw.get('orderBy', None)
+        if orderBy:
+            sql.append('order by')
+            sql.append(orderBy)
+        limit = kw.get('limit', None)
+        if limit is not None:
+            sql.append('limit')
+            if isinstance(limit, int):
+                sql.append('?')
+                args.append(limit)
+            elif isinstance(limit, tuple) and len(limit) == 2:
+                sql.append('?, ?')
+                args.extend(limit)
+            else:
+                raise ValueError('Invalid limit value: %s' % str(limit))
+        rs = await select(' '.join(sql), args)
+        return [cls(**r) for r in rs]
+
+    @classmethod
+    async def findNumber(cls, selectField, where=None, args=None):
+        """
+        :param selectField:
+        :param where:
+        :param args:
+        :return:
+        find number by select and where.
+        """
+        sql = ['select %s _num_ from `%s`' % (selectField, cls.__table__)]
+        if where:
+            sql.append('where')
+            sql.append(where)
+        rs = await select(' '.join(sql), args, 1)
+        if len(rs) == 0:
+            return None
+        return rs[0]['_num_']
+
+    @classmethod
+    async def find(cls, pk):
+        """
+        :param pk:
+        :return:
+        find object by primary key
+        """
+        rs = await select('%s where `%s`=?' % (cls.__select__, cls.__primary_key__), [pk], 1)
+        if len(rs) == 0:
+            return None
+        return cls(**rs[0])
+
+    async def save(self):
+        args = list(map(self.getValueOrDefault, self.__fields__))
+        args.append(self.getValueOrDefault(self.__primary_key__))
+        rows = await execute(self.__insert__, args)
+        if rows != 1:
+            logging.warning('failed to insert record: affected rows: %s' % rows)
+
+    async def update(self):
+        args = list(map(self.getValue, self.__fields__))
+        args.append(self.getValue(self.__primary_key__))
+        rows = await execute(self.__update__, args)
+        if rows != 1:
+            logging.warning('failed to update by primary key: affected rows: %s' % rows)
+
+    async def remove(self):
+        args = list(map(self.getValue, self.__fields__))
+        rows = await execute(self.__delete__, args)
+        if rows != 1:
+            logging.warning('failed to remove by primary key: affected rows:%s' % rows)
+
+
+
+
+
+
+
+
